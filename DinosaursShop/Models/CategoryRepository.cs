8777f212654aa8cee7d9c582325644ab0d6ed279
Кот Miseinen <miseinen22@gmail.com>
2020-06-23 02:40:00 +0300

    Add initial version of DinosaursShop. Add Dinosaur, Category models. Add ICategoryRepository, IDinosaurRepository interfaces. Implement ICategoryRepository

---
+++ DinosaursShop/Models/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinosaursShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> GetAllCategories => new List<Category>
+        {
+            new Category{CategoryId=1, CategoryName="Hard Dinosaur", CategoryDescription="Beautiful and Strong Hard Dinosaur"},
+            new Category{CategoryId=2, CategoryName="Light Dinosaur", CategoryDescription="Sweet and small Light Dinosaur"},
+            new Category{CategoryId=3, CategoryName="Middle Dinosaur", CategoryDescription="Awesome and interesting Middle Dinosaur"},
+        };
+    }
+}
---
+++ DinosaursShop/Models/IDinosaurRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinosaursShop.Models
+{
+    public interface IDinosaurRepository
+    {
+        IEnumerable<Dinosaur> GetAllDinosaurs { get; }
+        IEnumerable<Dinosaur> GetDinosaursOnSale { get; }
+        Dinosaur GetDinosaurById(int dinosaurId);
+    }
+}
