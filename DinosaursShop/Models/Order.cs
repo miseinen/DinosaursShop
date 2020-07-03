cd3706ea8ba837d144f64af807e84d9e11db6122
Кот Miseinen <miseinen22@gmail.com>
2020-07-03 17:36:31 +0300

    Add regular expressions validation to Order model.

--- DinosaursShop/Models/Order.cs
+++ DinosaursShop/Models/Order.cs
@@ -21,6 +21,7 @@ namespace DinosaursShop.Models
         /// </summary>
         [Required(ErrorMessage = "Please enter your first name")]
         [Display(Name = "First Name")]
+        [RegularExpression(@"^[a-zA-Z]*$", ErrorMessage = "Please use only Latin letters")]
         [StringLength(25)]
         public string FirstName { get; set; }
 
@@ -29,7 +30,8 @@ namespace DinosaursShop.Models
         /// </summary>
         [Required(ErrorMessage = "Please enter your last name")]
         [Display(Name = "Last Name")]
-        [StringLength(25)]
+        [RegularExpression(@"^[a-zA-Z]*$", ErrorMessage = "Please use only Latin letters")]
+        [StringLength(25, ErrorMessage = "Please use up to 25 letters")]
         public string LastName { get; set; }
 
         /// <summary>
@@ -37,33 +39,38 @@ namespace DinosaursShop.Models
         /// </summary>
         [Required(ErrorMessage = "Please enter your address")]
         [Display(Name = "Street Address")]
-        [StringLength(100)]
+        [StringLength(100, ErrorMessage = "Please use up to 100 letters")]
         public string Address { get; set; }
 
         /// <summary>
         /// Gets or sets a city.
         /// </summary>
         [Required(ErrorMessage = "Please enter your city")]
+        [RegularExpression(@"^[a-zA-Z]*$", ErrorMessage = "Please use only Latin letters")]
         public string City { get; set; }
 
         /// <summary>
         /// Gets or sets a sate.
         /// </summary>
         [Required(ErrorMessage = "Please enter your state")]
-        [StringLength(2, MinimumLength = 2)]
+        [RegularExpression(@"^[a-zA-Z]*$", ErrorMessage = "Please use only Latin letters")]
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "Please use up to 2 letters")]
         public string State { get; set; }
 
         /// <summary>
         /// Gets or sets a zipcode.
         /// </summary>
         [Required(ErrorMessage = "Please enter your zip code")]
-        [StringLength(5, MinimumLength = 5)]
+        [RegularExpression(@"^[1-9]*$", ErrorMessage = "Please use only digits")]
+        [StringLength(5, MinimumLength = 5, ErrorMessage = "Please use up to 5 letters")]
         public string ZipCode { get; set; }
 
         /// <summary>
         /// Gets or sets a phone number.
         /// </summary>
         [Required(ErrorMessage = "Please enter your phone number")]
+        [RegularExpression(@"^[+]?\d{3}[(]?\d{2}[)]?\d{3}[- .]?\d{4}$", ErrorMessage = "Please enter your phone number")]
+        [StringLength(20, MinimumLength = 12, ErrorMessage = "Please enter your phone number")]
         [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
 
